d08e29336a07c565998ba8b5f3af8cb8ad383a8c
Aminul Islam Alvi <aminulialvi@gmail.com>
2020-05-05 12:28:10 +0600

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,42 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios";
+import firebase from 'firebase/app';
 
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDsabTKKuCQBInmJ0kqUCmQUef7U8XXLAY",
+    authDomain: "vue-firebase-ef768.firebaseapp.com",
+    databaseURL: "https://vue-firebase-ef768.firebaseio.com",
+    projectId: "vue-firebase-ef768",
+    storageBucket: "vue-firebase-ef768.appspot.com",
+    messagingSenderId: "1043068005954",
+    appId: "1:1043068005954:web:a099f72774ab362760c7db",
+    measurementId: "G-14KXEZ1Z0K"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+  let app;
+
+  firebase.auth().onAuthStateChanged(user => {
+    console.log(user);
+    if (!app) {
+      app = new Vue({
+        router,
+        store,
+        render: h => h(App)
+      }).$mount("#app");
+    }
+  })
+  
+  // app = new Vue({
+  //   router,
+  //   store,
+  //   render: h => h(App)
+  // }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="This is a Simple Vuejs app with Firebase Authentication"/>
   </div>
 </template>
 
